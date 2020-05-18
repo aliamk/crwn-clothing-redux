8814266cb6ab6f07dbf6f3e23ce9ca50f59398ee
aliamk <alia.azarian@gmail.com>
2020-05-18 14:28:25 +0100

    updating the app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import HomePage from './pages/homepage.component'
@@ -46,16 +46,33 @@ class App extends React.Component {
       <Header/>
         <Switch>
           <Route exact path='/' component={ HomePage } />
-          <Route exact path='/shop' component={ ShopPage } />
-          <Route exact path='/signin' component={ SignInAndSignUpPage } />
+          <Route  path='/shop' component={ ShopPage } />
+          <Route 
+            exact path='/signin' 
+            render={() => 
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                < SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+// Redirect a signed-in user from the sign-in page
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(App);
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from './user.types'
+
 const INITIAL_STATE = {
   currentUser: null   // firebase's Google sign-in
 }
@@ -5,7 +7,7 @@ const INITIAL_STATE = {
 // using ES6's default parameter value for state
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload
